91c64c162181bc41f1571fa548eb1afd8c41fba3
perfectturmoil <perfectturmoil@gmail.com>
2014-10-02 22:07:05 -0400

    Mostly working find change

--- find_change.rb
+++ find_change.rb
@@ -1,7 +1,70 @@
-# Note: This didn't work very well, or at all. 
-
 require './extend_array'
 require './regression'
+require 'gruff'
+
+
+
+def find_change_points(y_axis, x_axis, number_of_chunks = 5, change_threshold = 1)
+  # Breaks y_axis and x_axis into chunks, finds slope of each chunk.
+  # Then compares slopes to determine if that chunk represents
+  # a change.
+  # Returns an array of the X coordinates at the begining of chunks identified
+
+  raise "not an array" unless y_axis.kind_of?(Array)
+  raise "not an array" unless x_axis.kind_of?(Array)
+
+  # Return variable
+  slope_change_x_values = []
+
+  chunk_length = (y_axis.length / number_of_chunks).floor
+
+  chunk_slopes= []
+
+  (0..number_of_chunks-1).each do |chunk|
+    start_point = chunk_length * chunk
+    # don't need end point
+    # end_point = chunk_length * chunk + chunk_length - 1
+
+    y_chunk = y_axis.slice(start_point, chunk_length)
+    x_chunk = x_axis.slice(start_point, chunk_length)
+    
+    chunk_regression = ArrayRegression.new(y_chunk, x_chunk)
+    chunk_slopes[chunk] = chunk_regression.slope
+  end
+
+  # puts chunk_slopes
+  # puts "\n\n"
+  # from here. Chunk slopes each_index do |slope_index|
+  # (actually 1..chunk_slopes.length -1).each do |slope_ index|
+  # if chunk_slopes[slope_index] different from chunk_slopes[index-1]
+  # maybe incorporate chunk_slopes [index+1] 
+  # then its an inflection, and document accordingly. Probably return
+  # the X value, maybe the index (may want to pivot the function to 
+    # use that rather than the chunk array thing - make it work first
+  
+  (1..number_of_chunks - 1).each do |chunk|
+    next_slope = chunk_slopes[chunk + 1] # Not used yet, might be better than this_chunk
+    this_slope = chunk_slopes[chunk]
+    prev_slope = chunk_slopes[chunk - 1]
+
+    # I need a better way of calculating change threshold.
+    # This way works, sorta - But having a fixed +/- isn't great, it probably needs
+    # to be a percent of itself, or something logarithmic. 
+
+    min_prev_slope = [prev_slope - change_threshold , prev_slope + change_threshold].min
+    max_prev_slope = [prev_slope - change_threshold , prev_slope + change_threshold].max
+
+    # puts "min prev: #{min_prev_slope}"
+    # puts "slope: #{this_slope}"
+    # puts "max prev: #{max_prev_slope}"
+    # puts "\n"
+
+    if not this_slope.between?(min_prev_slope, max_prev_slope)
+      slope_change_x_values << x_axis[chunk_length * chunk]
+    end
+  end
+  slope_change_x_values
+end
 
 noisy_decline = [10.3, 9.7, 8.9, 8.9, 8.9, 7.6, 7.8, 6.7, 6.4, 6.4, 6.5, 5.5, 5.4, 4.6, 4, 3.9, 3.1, 2.7, 3.1, 2.8, 2.1, 1.7, 0.9, 1, 0.3, 0.3,]
 clean_decline = noisy_decline.rolling_average(3)
@@ -10,42 +73,4 @@ noisy_knee = [10.2, 9.2, 9.4, 8.7, 8.4, 7.9, 6.7, 6.4, 5.6, 5.1, 5.2, 4.7, 3.7, 
 clean_knee = noisy_knee.rolling_average(3)
 x_axis = (0...26).to_a
 
-def find_change_points(signal, x_axis, number_of_chunks = 5, threshold = 0.5)
-  # Breaks array into chunks, compares the slopes. Returns an array of the index
-  # (start, middle) that it appears to change. 
-  # TODO: Consider implementing something R2'd
-  raise "signal not an array" unless signal.kind_of?(Array)
-  raise "x axis not an array" unless x_axis.kind_of?(Array)
-  change_indexes = []
-  length_of_chunk = (signal.length / number_of_chunks).floor
-
-  (1..number_of_chunks-2).each do |chunk_count| # Changed this to -2 from -1, because slope_stack[2] was NaN on last itteration
-    slope_stack = []
-    [-1, 0, 1].each do |offset|
-      test_signal = signal.slice( (chunk_count + offset) * length_of_chunk, length_of_chunk)
-      test_x = x_axis.slice( (chunk_count + offset) * length_of_chunk, length_of_chunk)
-      slope_stack << ArrayRegression.new(test_signal, test_x).slope 
-    end
-
-    pre_minus = slope_stack[0] - slope_stack[0] * threshold
-    pre_plus = slope_stack[0] + slope_stack[0] * threshold
-    post_minus = slope_stack[2] - slope_stack[2] * threshold
-    post_plus = slope_stack[2] + slope_stack[2] * threshold
-
-    puts "testing if #{slope_stack[1]} is bewteen #{pre_minus} and #{pre_plus}"
-    puts "then if #{slope_stack[1]} is bewteen #{post_minus} and #{post_plus}"
-    puts slope_stack[2]
-    # TODO: slopes being negative is screwing me up. Also, my slope numbers are real small, data may be too noisy?
-    if not slope_stack[1].between?(slope_stack[0] - slope_stack[0] * threshold, slope_stack[0] + slope_stack[0] * threshold)
-      if slope_stack[1].between?(slope_stack[2] - slope_stack[2] * threshold, slope_stack[2] + slope_stack[2] * threshold)
-        change_indexes << chunk_count * length_of_chunk
-      end
-    end 
-  end
-  
-  puts change_indexes
-end
-
-
-
-find_change_points(clean_knee, x_axis, 5, 0.5)
+puts find_change_points(clean_knee, x_axis, 4, 0.2)
--- test2.rb
+++ test2.rb
@@ -1,4 +1,8 @@
-my_array = [0,1,2,3,4,5,6,7,8,9,10]
+# my_array = [0,1,2,3,4,5,6,7,8,9,10]
 
-my_new_array = my_array.slice(0,5)
-puts my_new_array
+# my_new_array = my_array.slice(0,5)
+# puts my_new_array
+
+my_number = 5
+
+puts my_number.between?(10,-10)
